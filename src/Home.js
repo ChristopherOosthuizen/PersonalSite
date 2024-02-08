7db963374de47bf3a486674bb5cc8ad3b8e39a92
ChristopherOosthuizen <christopher.jan.oosthuizen@gmail.com>
2024-02-08 12:15:37 -0600

    fix

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ class Home extends React.Component {
         return (<div class="bg-mainpage w-full h-full bg-fill bg-center md:container md:mx-auto">
             <div class="text-center  h-screen flex-col flex flex-wrap items-center justify-center">
                <div class="  text-[#9a3412] text-5xl"> Chris Oosthuizen </div>
-               <div class="leading-5 text-[#52525b]">University of Houston Student and LLM Researcher at SAIL</div>
+               <div class="leading-5 text-[#52525b]">University of Houston Student and LLM Research Assistant at SAIL</div>
                
                <div class="w-96 p-3 text-sm"> Hi, I'm Chris Oosthuizen, Programming has been a lifelong passion, and I'm continually exploring new areas. Besides my academic pursuits, I'm an avid rock climber and am always excited to learn new things.</div>
                <div class=" p-12 gap-8 h-16 columns-3">
